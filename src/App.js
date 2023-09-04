e289e35f13c9dd4791203ac3d8d0bad617e655ba
celinemang <macaumang@gmail.com>
2023-09-04 13:00:11 -0500

    [2/n] Create TodoItem react component: Strikethrough todo item if marked finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+let todoItemId = 0;
+
 
 const TodoItemInputField = (props) => {
-  return (<div>  hi </div>);
+  const [input, setInput] = useState("");
+
+  const onSubmit = () => {
+        props.onSubmit(input);
+        setInput("");
+      };
+    
+  return (<div>   
+    <TextField  id="todo-item-input"
+      label="Todo Item"
+      variant="outlined"
+     onChange={(e) => setInput(e.target.value)} value={input}
+    />
+    <Button variant="outlined" onClick = {onSubmit}>Submit</Button>
+  </div>);
   };
   
-  
+  const TodoItem = (props) => {
+    const style = props.todoItem.isFinished ? { textDecoration: 'line-through' } : {};
+      return (<li>
+        <span style={style} onClick={() => props.onTodoItemClick(props.todoItem)}>
+          {props.todoItem.todoItemContent}</span>
+      </li>);
+    };
+    
+
+  const TodoItemList = (props) => {
+    const todoList = props.todoItemList.map((todoItem, index)=>{
+      return <TodoItem key={index} todoItem={todoItem} onTodoItemClick={props.onTodoItemClick} />;
+    });
+      return (<div>
+        <u1>{todoList}</u1>
+
+      </div>);
+    };
+    
+
 function App() {
+  const [todoItemList, setTodoItemList] = useState([]);
+  const onSubmit = (newTodoItem) => {
+        setTodoItemList([...todoItemList, {
+          id: todoItemId++,
+          todoItemContent: newTodoItem,
+          isFinished: false,
+        }]);
+      };
+   
+  const onTodoItemClick = (clickedTodoItem) => {
+    setTodoItemList(todoItemList.map((todoItem) => {
+      if (clickedTodoItem.id === todoItem.id) {
+        return {
+          id: clickedTodoItem.id,
+          todoItemContent: clickedTodoItem.todoItemContent,
+          isFinished: !clickedTodoItem.isFinished,
+        };
+      } else {
+        return todoItem;
+      }
+    }));
+  };
+
+    
   return (
     <div className="App">
-      <TodoItemInputField />
+      <TodoItemInputField onSubmit={onSubmit} /> 
+      <TodoItemList  todoItemList ={todoItemList}/>
+      <TodoItemList
+        todoItemList={todoItemList}
+        onTodoItemClick={onTodoItemClick}
+     />
+
     </div>
   );
 }
